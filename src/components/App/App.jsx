a45346bdf1af6e98bfb226de6e498e03d8e2c11e
Jacob Larson <hockeybobber@comcast.net>
2024-08-12 21:59:29 -0500

    changed names

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,7 @@ function App (){
   return(
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to Your Garden!</h1>
+        <h1>Welcome to React!</h1>
       </header>
       <Garden />
     </div>

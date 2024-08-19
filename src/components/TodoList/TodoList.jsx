9836c8ef427ca5096c947f00d43ffc7ab8a16fbd
Jacob Larson <hockeybobber@comcast.net>
2024-08-19 18:18:01 -0500

    deleted useless code

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -5,16 +5,13 @@ import { useDispatch, useSelector} from 'react-redux';
 function TodoList() {
     const dispatch = useDispatch();
 
-    const reduxState = useSelector(store => store);
-
     useEffect(() => {
-        // dispatch an action to request the plantList from the API
+
     }, []); 
 
     return (
         <div>
             <h3>This is the todo list</h3>
-            <pre>{JSON.stringify(reduxState)}</pre>
         </div>
     );
 }

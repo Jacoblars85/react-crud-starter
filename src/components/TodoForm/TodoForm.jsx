c912fd725556a7191b6a7df79102e71a6042d223
Jacob Larson <hockeybobber@comcast.net>
2024-08-19 18:16:26 -0500

    changed names and deleted old code

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -5,25 +5,20 @@ const TodoForm = () => {
     const dispatch = useDispatch();
     
     //Initial state is an OBJECT, with keys id and name
-    let [newPlant, setPlant] = useState({id: 4, name: ''});
+    let [newTodo, setTodo] = useState({id: 4, name: ''});
 
     const handleNameChange = (event) => {
-        //Similar to in redux -- we dont want to get rid of the id field when we update name
-        setPlant({...newPlant, name: event.target.value})
+
     }
 
-    const addNewPlant = event => {
-        event.preventDefault();
-        dispatch({ type: 'ADD_PLANT', payload: newPlant });
-        //updates the next plant to have a new id
-        setPlant({id:newPlant.id + 1, name: ''});
+    const addNewTodo = event => {
+
     }
     return (
         <div>
             <h3>This is the form</h3>
-            <pre>{JSON.stringify(newPlant)}</pre>
-            <form onSubmit={addNewPlant}>
-                <input type='text' value={newPlant.name} onChange={handleNameChange} />
+            <pre>{JSON.stringify(newTodo)}</pre>
+            <form onSubmit={addNewTodo}>
                 <input type='submit' value='Add New todo' />
             </form>
         </div>

3ec009aa3eb5ef22e83805a7f5f872f1023c72b2
Jacob Larson <hockeybobber@comcast.net>
2024-08-23 20:11:08 -0500

    deleted usless code

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -3,19 +3,12 @@ import { useDispatch } from 'react-redux';
 
 const TodoForm = () => {
     const dispatch = useDispatch();
-    
-    //Initial state is an OBJECT, with keys id and name
-    let [newTodo, setTodo] = useState();
-
-    const addNewTodo = event => {
-
-    }
 
     return (
         <div>
             <h3>This is the form</h3>
 
-            <p>{newTodo}</p>
+            <p>type stuff here</p>
         </div>
     );
 }

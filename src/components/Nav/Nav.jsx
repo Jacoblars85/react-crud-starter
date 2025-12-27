dc82a50fd502029e047e1ce4262590db4cc4f5f2
Jacob Larson <hockeybobber@comcast.net>
2025-12-27 16:27:59 -0600

    added nav code

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,31 @@
+import React, { useState, forwardRef } from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+import { useSelector, useDispatch } from "react-redux";
+
+
+
+
+
+function Nav(props) {
+  const dispatch = useDispatch();
+
+
+
+  return (
+    <div className="nav">
+      <Link to="/home">
+        <h2 className="nav-title">Bowsermon</h2>
+      </Link>
+      {/* If no user is logged in, show these links */}
+      {!user.id && (
+        // If there's no user, show login/registration links
+        <Link className="navLink" to="/login">
+          Login / Register
+        </Link>
+      )}
+    </div>
+  );
+}
+
+export default Nav;

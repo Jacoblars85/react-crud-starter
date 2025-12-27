622437afa8369c32fe266a7aa67ce2982adab79c
Jacob Larson <hockeybobber@comcast.net>
2025-12-27 16:29:38 -0600

    formatted

--- src/components/BasicForm/BasicForm.jsx
+++ src/components/BasicForm/BasicForm.jsx
@@ -1,19 +1,18 @@
-import React, { useState }from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
 const BasicForm = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-      const store = useSelector((store) => store);
+  const store = useSelector((store) => store);
 
-    return (
-        <div>
-            <h3>This is the form</h3>
+  return (
+    <div>
+      <h3>This is the form</h3>
 
-            <p>type stuff here</p>
-        </div>
-    );
-}
-
+      <p>type stuff here</p>
+    </div>
+  );
+};
 
 export default BasicForm;

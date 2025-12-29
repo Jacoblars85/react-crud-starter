2c050afb459f63620ea2638055efde304c527ae0
Jacob Larson <hockeybobber@comcast.net>
2025-12-28 19:13:34 -0600

    fixed the nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./Nav.css";
 import { useSelector, useDispatch } from "react-redux";
 
-function Nav(props) {
+function Nav() {
   const dispatch = useDispatch();
 
   const store = useSelector((store) => store);
@@ -11,15 +11,8 @@ function Nav(props) {
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Bowsermon</h2>
+        <h2 className="nav-title">Title</h2>
       </Link>
-      {/* If no user is logged in, show these links */}
-      {!user.id && (
-        // If there's no user, show login/registration links
-        <Link className="navLink" to="/login">
-          Login / Register
-        </Link>
-      )}
     </div>
   );
 }
